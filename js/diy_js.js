192460c5f473388466c07e8761c723a0d8179991
root apostolos <info.interpedia@gmail.com>
2014-10-25 09:20:42 -0400

    add wss

--- js/diy_js.js
+++ js/diy_js.js
@@ -1,12 +1,13 @@
 function diy_tools () {
-	var diy__hostname = "your server";
-     	this.https_url = "https://"+diy__hostname;
-     	this.wss_url = "wss://"+diy__hostname;
-     	this.client_id = "CLIENT_ID11";
-     	this.client_secret = "CLIENT_SECRET11";
-     	this.device = "kittensCategory";
+        var diy__hostname = "arduino.os.cs.teiath.gr";
+        this.https_url = "https://"+diy__hostname;
+        this.wss_url = "wss://"+diy__hostname;
+        this.client_id = "CLIENT_ID11";
+        this.client_secret = "arduino#c%cf!q";
+        this.device = "";
 }
-
+// ***GIT*** 
+// ***GitGit*** 
 // var data="grant_type=client_credentials&client_id="+username+"&client_secret="+password;
 /*
  * get token from server
@@ -61,8 +62,12 @@ diy_tools.prototype.wss_connect = function()  {
     	var subject = this;
 	var conn = new ab.Session(this.wss_url+'?access_token='+this.access_token,
 		function() {
+			var device = new Object();
+			device.access_token = subject.access_token;
+			device.name = subject.device;
+			devicestr = JSON.stringify(device);
 			conn.subscribe(subject.device, function(topic, data) {
-				console.log('New device view"' + topic + '" : ' + data.category);
+				console.log('device data:"' + topic + '" : ' + data.a+data.b+data.c);
 			});
 		},
 		function() {
@@ -74,6 +79,22 @@ diy_tools.prototype.wss_connect = function()  {
 	);
 }
 
+/*
+ * get devices from server
+ * required access_token
+ * return user devices 
+ */
+diy_tools.prototype.getDevices = function()  {
+    	var subject = this;
+	return $.ajax({
+		type: "GET",
+		url: this.https_url+'/api/devices',
+		dataType: "json",
+		data: {
+			'access_token': this.access_token
+		}
+	});
+}
 
 
 
