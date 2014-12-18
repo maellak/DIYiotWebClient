c2766ac3aa04610684e7c3a5583b0427aabd7422
datapack <demos@datapack.gr>
2014-12-18 13:45:08 +0200

    prosthiki login dialog

--- js/diy_js.js
+++ js/diy_js.js
@@ -39,17 +39,65 @@ diy_tools.prototype.getToken = function()  {
 
 */
 diy_tools.prototype.getToken = function()  {
-    	var subject = this;
-	return $.ajax({
-		type: "POST",
-		url: this.https_url+'/api/token',
-		dataType: "json",
-		data: {
-			'grant_type': 'client_credentials', 
-			'client_id': this.client_id, 
-			'client_secret': this.client_secret
-		}
-	});
+    var subject = this;
+    var dfd = $.Deferred();
+    var performAjaxRequest = function() {
+        return $.ajax({
+                type: "POST",
+                url: subject.https_url+'/api/token',
+                dataType: "json",
+                data: {
+                    'grant_type': 'client_credentials', 
+                    'client_id': subject.client_id, 
+                    'client_secret': subject.client_secret
+                }
+        });
+    }
+    // If no credentials are saved show a modal dialog
+    var savedUsername = localStorage.getItem("username");
+    var savedPassword = localStorage.getItem("password");
+    if(savedUsername == null || savedPassword == null) {
+        $("<div><div><input id='username' type='text' placeholder='Username' /></div><div><input id='password' type='password' placeholder='Password' /></div></div>").dialog({
+            dialogClass: "no-close",
+            buttons: [
+                {
+                    text: "Login",
+                    click: function() {
+                        subject.client_id = $(this).find('#username').val();
+                        subject.client_secret = $(this).find('#password').val();
+                        var $this = $(this);
+                        performAjaxRequest().done(function(data) {
+                            // Save them in storage
+                            if(typeof data.access_token != 'undefined') {
+                                $this.dialog( "close" );
+                                localStorage.setItem("username", subject.client_id);
+                                localStorage.setItem("password", subject.client_secret);
+                                dfd.resolve(data);
+                            } else {
+                                alert('Invalid credentials');
+                            }
+                        });
+                    }
+                }
+            ]
+        });
+    } else {
+        subject.client_id = savedUsername;
+        subject.client_secret = savedPassword;
+        performAjaxRequest().done(function(data) {
+            if(typeof data.access_token != 'undefined') {
+                dfd.resolve(data);
+            } else {
+                subject.client_id = null;
+                subject.client_secret = null;
+                localStorage.removeItem("username");
+                localStorage.removeItem("password");
+                var newdfd = subject.getToken();
+                newdfd.done(function(data) { dfd.resolve(data); });
+            }
+        });
+    }
+    return dfd;
 }
 
 /*
